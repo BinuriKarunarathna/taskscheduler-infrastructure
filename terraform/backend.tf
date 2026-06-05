0a9d52ce6d695efd3a23575461e91c0d4514ffb7
Binuri Karunarathna <154871134+BinuriKarunarathna@users.noreply.github.com>
2026-06-06 04:52:13 +0530

    jenkins changed

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,8 @@
 terraform {
-  backend "s3" {
-    bucket         = "taskmanager-tf-state"       # Create this S3 bucket in AWS first
-    key            = "state/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "taskmanager-terraform-locks" # Create this DynamoDB table in AWS first
-    encrypt        = true
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
